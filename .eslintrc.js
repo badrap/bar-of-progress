ab5f22e2d85342a9f87a57eeefb7ef3711fa6898
Joachim Viide <jviide@iki.fi>
2022-05-14 22:34:56 +0000

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,34 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/@typescript-eslint",
-  ],
+  root: true,
+  extends: ["plugin:prettier/recommended"],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: "module",
-    project: "./tsconfig.json",
   },
-  plugins: ["prettier"],
   rules: {
     "prettier/prettier": "warn",
     "linebreak-style": ["error", "unix"],
     eqeqeq: ["error", "smart"],
-    "@typescript-eslint/no-for-in-array": ["error"],
-    "@typescript-eslint/explicit-member-accessibility": [
-      "error",
-      { accessibility: "no-public" },
-    ],
-    "@typescript-eslint/explicit-function-return-type": [
-      "error",
-      { allowExpressions: true, allowTypedFunctionExpressions: true },
-    ],
   },
+  overrides: [
+    {
+      files: ["src/**/*.{ts,tsx}"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      extends: ["plugin:@typescript-eslint/recommended"],
+      rules: {
+        "@typescript-eslint/no-for-in-array": ["error"],
+        "@typescript-eslint/explicit-member-accessibility": [
+          "error",
+          { accessibility: "no-public" },
+        ],
+        "@typescript-eslint/explicit-function-return-type": [
+          "error",
+          { allowExpressions: true, allowTypedFunctionExpressions: true },
+        ],
+      },
+    },
+  ],
 };
